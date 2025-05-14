3fdcce997eea6f488d3f03fd4807665fccaf5c11
Park, Daekyeong <pdk2037@gmail.com>
2025-05-14 23:40:29 +0900

    자동화 테스트

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/")
     public String hello() {
-        return "Hello World";
+        return "테스트";
     }
 }
