5589e34ef5822b5a832bad0791787bb1e6061759
Park, Daekyeong <pdk2037@gmail.com>
2025-06-03 23:57:15 +0900

    cicd test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String hello() {
         return "테스트";
     }
